7aa9395fa995f99cc2144252b14aa65d3899f6c8
AndreyKapus <kapustnikov@ukr.net>
2023-05-07 13:24:17 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import NavBar from "./Components/NavBar/NavBar";
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <div>
+            <h1>Contacts</h1>
+            <button>Sign up</button>
+            <button>Login</button>
+        </div>
+    )
+};
+
+export default NavBar;
