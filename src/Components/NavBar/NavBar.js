512914cc76fec7fd40f2a81ddf439007ec5f82da
AndreyKapus <kapustnikov@ukr.net>
2023-05-08 12:28:57 +0300

    store

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,8 @@
-import { SignUpForm } from "../SignUp/SignUp";
-
 export const NavBar = () => {
     return (
         <div>
             <h1>Contacts</h1>
             <button type="button">Sign up</button>
-            <SignUpForm/>
         </div>
     )
 };
