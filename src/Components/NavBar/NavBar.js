f1b1091cae1f7409468ba2548d3c1937729f3fe6
AndreyKapus <kapustnikov@ukr.net>
2023-05-08 12:35:43 +0300

    nav

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 export const NavBar = () => {
     return (
         <div>
             <h1>Contacts</h1>
-            <button type="button">Sign up</button>
+            <NavLink to='/signup'>Sign up</NavLink>
+            <br/>
+            <NavLink to='login'>Login</NavLink>
         </div>
     )
 };
