6955784359eb75b43075a37d27a00f96a47f4365
AndreyKapus <kapustnikov@ukr.net>
2023-05-08 13:18:27 +0300

    operations

---
+++ src/Redux/Operations.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.default.baseURL = 'https://https://connections-api.herokuapp.com';
+
+const register = createAsyncThunk(
+    'auth/register',
+    async credentials => {
+        try{
+            const {data} = axios.post('users/signup', credentials);
+            return data;
+        } catch (error) {
+            return error.message;
+        }
+    }
+)
